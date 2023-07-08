795427541722542c9abb5d8570157ef03ea10972
RubaNaseem <139000850+RubaNaseem@users.noreply.github.com>
2023-07-08 22:03:56 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objective**
+# * Languages for Data Science
+# * Tools for Data Science
+# * Libraries of python
+# * Introduction to Jupyter
+
+# # Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Scala
+# 4. Julia
+# 
+
+# # Some of the commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Scipy
+# 3. Pandas
+# 4. Scikit-learn
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Ruba Naseem
+
+# In[ ]:
+
+
+
+
